585f4c8e563cf0c8a5b120cb101d1c0c75745dc6
Larry Bellou <larrybellou@gmail.com>
2021-12-27 13:41:28 -0600

    Added database connection and scaffolded database

---
+++ BookStoreApp.API/Data/BookstoreContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BookStoreApp.API.Data
+{
+    public partial class BookstoreContext : DbContext
+    {
+        public BookstoreContext()
+        {
+        }
+
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Firstname).HasMaxLength(50);
+
+                entity.Property(e => e.Lastname).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Image).HasMaxLength(150);
+
+                entity.Property(e => e.Isbn)
+                    .HasMaxLength(50)
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.Property(e => e.Summary).HasMaxLength(150);
+
+                entity.Property(e => e.Title).HasMaxLength(100);
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId)
+                    .HasConstraintName("FK_Books_Authors");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
